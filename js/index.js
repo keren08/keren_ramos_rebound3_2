8dc532ae37e7d9afe9a1b91d22d0f40e45ac73b3
keren ramos <keren.ramoss@gmail.com>
2022-12-05 20:03:39 -0600

    ejercicio final

--- js/index.js
+++ js/index.js
@@ -3,9 +3,13 @@ const parrafo = document.getElementById("parrafo");
 
 
 document.addEventListener("click",(e)=>{
+    e.preventDefault();
     const html = generarHTML();
+
+    if(!document.getElementById('card')){
     contenido.innerHTML = "";
     contenido.insertAdjacentHTML("afterbegin",html);
+  }
 
     if(e.target.id == "borrar") {
       let confirma  = confirm("¿Seguro desea eliminar nota?");
@@ -17,8 +21,8 @@ document.addEventListener("click",(e)=>{
     if(e.target.id == "agregar") {
       if(!document.getElementById('fecha')){
         const nota = document.getElementById("notaC");
-        const html = `<section>
-              <input type="date" name="fecha" id="fecha" />
+        const html = `<section class='form__seccion'>
+              <input type="date" name="fecha"  class='form__seccion__input' id="fecha" />
              </section>`;
          nota.insertAdjacentHTML("beforeend",html);
      }
@@ -32,32 +36,34 @@ function generarHTML(){
  const card = `
  <main id="contenido">
  <div id="card">
- <form action="">
-   <section>
+ <form action="" class='form'>
+   <section class='form__seccion'>
      <input
        type="text"
        name="titulo"
+       class='form__seccion__input'
        id="titulo"
        placeholder="Ingresa un titulo"
      />
    </section>
 
-   <section id="notaC">
+   <section id="notaC" class='form__seccion'>
      <textarea
        name="nota"
        id=""
+       class='form__seccion__input'
        cols="30"
        rows="10"
        placeholder="Escribe aqui"
      ></textarea>
    </section>
 
-   <section>
-     <button id="borrar" type="submit">
+   <section class='form__seccion'>
+     <button id="borrar" class='form__boton' type="submit">
        <i class="fa fa-trash" aria-hidden="true"></i>Borrar nota
      </button>
    
-     <button id="agregar"  type="submit">
+     <button id="agregar" class='form__boton' type="submit">
        <i class="fa fa-calendar" aria-hidden="true"></i>Agregar Fecha
      </button>
    </section>
